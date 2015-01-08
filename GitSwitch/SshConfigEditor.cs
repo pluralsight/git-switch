9af3d2e9303990437bebdf9ae3e2443eeadddf3a
Allan Stewart <allan-stewart@pluralsight.com>
2015-01-07 17:13:52 -0700

    Created class to edit the SSH config file.

---
+++ GitSwitch/SshConfigEditor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GitSwitch
+{
+    public class SshConfigEditor
+    {
+        private IFileHandler fileHandler;
+
+        public string SshConfigFilePath
+        {
+            get
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\.ssh\config";
+            }
+        }
+
+        public SshConfigEditor(IFileHandler fileHandler)
+        {
+            this.fileHandler = fileHandler;
+        }
+
+        public void SetGitHubKeyFile(string sshKeyPath)
+        {
+            string unixSshKeyPath = WindowsToUnixPath(sshKeyPath);
+            string defaultSshConfig = "Host github.com\n\tIdentityFile " + unixSshKeyPath + "\n";
+            
+            try
+            {
+                IEnumerable<string> configFileLines = fileHandler.ReadLines(SshConfigFilePath);
+                List<string> output = new List<string>();
+                bool didFindGitHub = false;
+                bool inGitHubSection = false;
+
+                foreach (var line in configFileLines)
+                {
+                    output.Add(line);
+                    if (Regex.IsMatch(line, @"^\s*Host\s+"))
+                    {
+                        inGitHubSection = line.Contains("github.com");
+                        if (inGitHubSection)
+                        {
+                            didFindGitHub = true;
+                            output.Add("\tIdentityFile " + unixSshKeyPath);
+                        }
+                    }
+                    if (inGitHubSection && Regex.IsMatch(line, @"^\s*IdentityFile\s+"))
+                    {
+                        output.RemoveAt(output.Count - 1);
+                    }
+                }
+
+                if (!didFindGitHub)
+                {
+                    if (output[output.Count - 1] != "")
+                    {
+                        output.Add("");
+                    }
+                    output.Add(defaultSshConfig);
+                }
+
+                fileHandler.WriteFile(SshConfigFilePath, string.Join("\n", output));
+            }
+            catch (FileNotFoundException)
+            {
+                fileHandler.WriteFile(SshConfigFilePath, defaultSshConfig);
+            }
+        }
+
+        private string WindowsToUnixPath(string path)
+        {
+            return Regex.Replace(path.Replace("\\", "/"), "^([A-Za-z]):", "/$1");
+        }
+    }
+}
---
+++ UnitTests/SshConfigEditorTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitSwitch;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class SshConfigEditorTests
+    {
+        private SshConfigEditor editor;
+        private Mock<IFileHandler> mockFileHandler;
+        private string testKeyWindowsPath = @"C:\Users\fakeuser\.ssh\githubkey";
+        private string testKeyUnixPath = @"/C/Users/fakeuser/.ssh/githubkey";
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockFileHandler = new Mock<IFileHandler>(MockBehavior.Strict);
+
+            editor = new SshConfigEditor(mockFileHandler.Object);
+        }
+
+        [Test]
+        public void ItWritesANewFileIfOneWasNotFound()
+        {
+            string expectedData = "Host github.com\n\tIdentityFile " + testKeyUnixPath + "\n";
+            
+            mockFileHandler.Setup(mock => mock.ReadLines(It.IsAny<string>())).Throws(new FileNotFoundException());
+            mockFileHandler.Setup(mock => mock.WriteFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((path, data) =>
+                {
+                    Assert.AreEqual(editor.SshConfigFilePath, path);
+                    Assert.AreEqual(expectedData, data);
+                });
+
+            editor.SetGitHubKeyFile(testKeyWindowsPath);
+
+            mockFileHandler.Verify(mock => mock.ReadLines(editor.SshConfigFilePath));
+            mockFileHandler.Verify(mock => mock.WriteFile(editor.SshConfigFilePath, expectedData));
+        }
+
+        [Test]
+        public void ItAddsTheGitHubEntryIfItWasNotFound()
+        {
+            List<string> priorConfigLines = GetExampleConfigLines();
+            string expectedData = string.Join("\n", priorConfigLines) + "\nHost github.com\n\tIdentityFile " + testKeyUnixPath + "\n";
+            
+            mockFileHandler.Setup(mock => mock.ReadLines(It.IsAny<string>())).Returns(priorConfigLines);
+            mockFileHandler.Setup(mock => mock.WriteFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((path, data) =>
+                {
+                    Assert.AreEqual(editor.SshConfigFilePath, path);
+                    Assert.AreEqual(expectedData, data);
+                });
+
+            editor.SetGitHubKeyFile(testKeyWindowsPath);
+
+            mockFileHandler.Verify(mock => mock.ReadLines(editor.SshConfigFilePath));
+            mockFileHandler.Verify(mock => mock.WriteFile(editor.SshConfigFilePath, expectedData));
+        }
+
+        [Test]
+        public void ItChangesTheGitHubIdentityFile()
+        {
+            List<string> priorConfigLines = GetExampleConfigLines("~/.ssh/oldGitHubKey");
+            string expectedData = string.Join("\n", GetExampleConfigLines(testKeyUnixPath));
+
+            mockFileHandler.Setup(mock => mock.ReadLines(It.IsAny<string>())).Returns(priorConfigLines);
+            mockFileHandler.Setup(mock => mock.WriteFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((path, data) =>
+                {
+                    Assert.AreEqual(editor.SshConfigFilePath, path);
+                    Assert.AreEqual(expectedData, data);
+                });
+
+            editor.SetGitHubKeyFile(testKeyWindowsPath);
+
+            mockFileHandler.Verify(mock => mock.ReadLines(editor.SshConfigFilePath));
+            mockFileHandler.Verify(mock => mock.WriteFile(editor.SshConfigFilePath, expectedData));
+        }
+
+        private List<string> GetExampleConfigLines(string gitHubKey = null)
+        {
+            var lines = new List<string>();
+
+            lines.Add("Host bitbucket.com");
+            lines.Add("\tIdentityFile ~/.ssh/bitbucket");
+            lines.Add("");
+
+            if (gitHubKey != null)
+            {
+                lines.Add("Host github.com");
+                lines.Add("\tIdentityFile " + gitHubKey);
+                lines.Add("");
+            }
+
+            lines.Add("Host example.com");
+            lines.Add("\tStrictHostKeyChecking no");
+            lines.Add("");
+
+            return lines;
+        }
+    }
+}
