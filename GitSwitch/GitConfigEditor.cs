6e92002227579bd88fafe885cebe97894f225cb1
Allan Stewart <allan-stewart@pluralsight.com>
2015-01-08 15:15:33 -0700

    Added a class to update the ~/.gitconfig file.

---
+++ GitSwitch/GitConfigEditor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GitSwitch
+{
+    public class GitConfigEditor
+    {
+        private IFileHandler fileHandler;
+
+        public GitConfigEditor(IFileHandler fileHandler)
+        {
+            this.fileHandler = fileHandler;
+        }
+
+        public string ConfigFilePath
+        {
+            get
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\.gitconfig";
+            }
+        }
+
+        public void SetGitUsernameAndEmail(string username, string email)
+        {
+            string newConfig;
+
+            try
+            {
+                IEnumerable<string> configLines = fileHandler.ReadLines(ConfigFilePath);
+                newConfig = ProcessConfigFile(configLines, username, email);
+            }
+            catch (FileNotFoundException)
+            {
+                newConfig = string.Format("[user]\n\tname = {0}\n\temail = {1}\n", username, email);
+            }
+
+            fileHandler.WriteFile(ConfigFilePath, newConfig);
+        }
+
+        private string ProcessConfigFile(IEnumerable<string> configLines, string username, string email)
+        {
+            var output = new List<string>();
+            bool didFindUserSection = false;
+            bool inUserSection = false;
+
+            foreach (var line in configLines)
+            {
+                output.Add(line);
+                if (Regex.IsMatch(line, @"^\s*\["))
+                {
+                    inUserSection = Regex.IsMatch(line, @"^\s*\[user\]");
+                    if (inUserSection)
+                    {
+                        didFindUserSection = true;
+                        output.Add("\tname = " + username);
+                        output.Add("\temail = " + email);
+                    }
+                }
+                if (inUserSection && Regex.IsMatch(line, @"^\s*(name|email)\s+=\s+"))
+                {
+                    output.RemoveAt(output.Count - 1);
+                }
+            }
+
+            if (!didFindUserSection)
+            {
+                if (output[output.Count - 1] == "")
+                {
+                    output.RemoveAt(output.Count - 1);
+                }
+                output.Add("[user]");
+                output.Add("\tname = " + username);
+                output.Add("\temail = " + email);
+                output.Add("");
+            }
+            return string.Join("\n", output);
+        }
+    }
+}
---
+++ UnitTests/GitConfigEditorTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitSwitch;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class GitConfigEditorTests
+    {
+        private const string TestUsername = "John Doe";
+        private const string TestEmail = "john-doe@example.com";
+
+        private GitConfigEditor editor;
+        private Mock<IFileHandler> mockFileHandler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockFileHandler = new Mock<IFileHandler>(MockBehavior.Strict);
+
+            editor = new GitConfigEditor(mockFileHandler.Object);
+        }
+
+        [Test]
+        public void ItWritesANewConfigFileIfNoneWasFound()
+        {
+            string expectedData = string.Format("[user]\n\tname = {0}\n\temail = {1}\n", TestUsername, TestEmail);
+            mockFileHandler.Setup(mock => mock.ReadLines(It.IsAny<string>())).Throws(new FileNotFoundException());
+            mockFileHandler.Setup(mock => mock.WriteFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((path, data) =>
+                {
+                    Assert.AreEqual(editor.ConfigFilePath, path);
+                    Assert.AreEqual(expectedData, data);
+                });
+
+            editor.SetGitUsernameAndEmail(TestUsername, TestEmail);
+
+            mockFileHandler.Verify(mock => mock.ReadLines(editor.ConfigFilePath));
+            mockFileHandler.Verify(mock => mock.WriteFile(editor.ConfigFilePath, expectedData));
+        }
+
+        [Test]
+        public void ItAddsTheUserSectionIfNotFound()
+        {
+            string expectedData = string.Join("\n", GetExampleGitConfig()) +
+                string.Format("[user]\n\tname = {0}\n\temail = {1}\n", TestUsername, TestEmail);
+            mockFileHandler.Setup(mock => mock.ReadLines(It.IsAny<string>())).Returns(GetExampleGitConfig());
+            mockFileHandler.Setup(mock => mock.WriteFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((path, data) =>
+                {
+                    Assert.AreEqual(editor.ConfigFilePath, path);
+                    Assert.AreEqual(expectedData, data);
+                });
+
+            editor.SetGitUsernameAndEmail(TestUsername, TestEmail);
+
+            mockFileHandler.Verify(mock => mock.ReadLines(editor.ConfigFilePath));
+            mockFileHandler.Verify(mock => mock.WriteFile(editor.ConfigFilePath, expectedData));
+        }
+
+        [Test]
+        public void ItChangesTheUsernameAndEmailIfFound()
+        {
+            string expectedData = string.Join("\n", GetExampleGitConfig(TestUsername, TestEmail));
+            mockFileHandler.Setup(mock => mock.ReadLines(It.IsAny<string>())).Returns(GetExampleGitConfig("oldUser", "oldEmail"));
+            mockFileHandler.Setup(mock => mock.WriteFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((path, data) =>
+                {
+                    Assert.AreEqual(editor.ConfigFilePath, path);
+                    Assert.AreEqual(expectedData, data);
+                });
+
+            editor.SetGitUsernameAndEmail(TestUsername, TestEmail);
+
+            mockFileHandler.Verify(mock => mock.ReadLines(editor.ConfigFilePath));
+            mockFileHandler.Verify(mock => mock.WriteFile(editor.ConfigFilePath, expectedData));
+        }
+
+        private IEnumerable<string> GetExampleGitConfig(string username = null, string email = null)
+        {
+            var configLines = new List<string>();
+            configLines.Add("[core]");
+            configLines.Add("\tautocrlf = false");
+            if (username != null || email != null)
+            {
+                configLines.Add("[user]");
+                if (username != null)
+                {
+                    configLines.Add("\tname = " + username);
+                }
+                if (email != null)
+                {
+                    configLines.Add("\temail = " + email);
+                }
+            }
+            configLines.Add("[push]");
+            configLines.Add("\tdefault = simple");
+            configLines.Add("");
+            return configLines;
+        }
+    }
+}
