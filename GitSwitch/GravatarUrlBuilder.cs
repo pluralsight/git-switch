ebd927e3570cb51b27d1110627b94812ecfa342c
Eric Andres <emandres@gmail.com>
2015-04-29 10:01:36 -0600

    Format Gravatar URLs

---
+++ GitSwitch/GravatarUrlBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GitSwitch
+{
+    public class GravatarUrlBuilder
+    {
+        public string NormalizeEmail(string email)
+        {
+            return email.Trim().ToLower();
+        }
+
+        public string HashEmail(string normalizedEmail)
+        {
+            var hashBytes = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(normalizedEmail));
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+
+        public string GetUrlForEmail(string email)
+        {
+            var hash = HashEmail(NormalizeEmail(email));
+            return string.Format("http://gravatar.com/avatar/{0}?s=64&r=g", hash);
+        }
+    }
+}
---
+++ UnitTests/GravatarUrlBuilderTests.cs
@@ -0,0 +1,58 @@
+﻿using GitSwitch;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class GravatarUrlBuilderTests
+    {
+        private GravatarUrlBuilder ClassUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            ClassUnderTest = new GravatarUrlBuilder();
+        }
+
+        [Test]
+        public void NormalizedEmailsContainNoSpaces()
+        {
+            var input = " test@example.com ";
+
+            var output = ClassUnderTest.NormalizeEmail(input);
+
+            Assert.That(output, Is.EqualTo("test@example.com"));
+        }
+
+        [Test]
+        public void NormalizedEmailsAreLowerCase()
+        {
+            var input = "Test@Example.com";
+
+            var output = ClassUnderTest.NormalizeEmail(input);
+
+            Assert.That(output, Is.EqualTo("test@example.com"));
+        }
+
+
+        [Test]
+        public void EmailsAreMd5Hashed()
+        {
+            var input = "test@example.com";
+
+            var output = ClassUnderTest.HashEmail(input);
+
+            Assert.That(output, Is.EqualTo("55502f40dc8b7c769880b10874abc9d0"));
+        }
+
+        [Test]
+        public void UrlsAreProperlyFormatted()
+        {
+            var input = " Test@Example.com ";
+
+            var output = ClassUnderTest.GetUrlForEmail(input);
+
+            Assert.That(output, Is.EqualTo("http://gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0?s=64&r=g"));
+        }
+    }
+}
